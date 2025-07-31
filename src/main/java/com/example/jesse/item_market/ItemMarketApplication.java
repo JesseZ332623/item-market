0a73d96e47ef874818ef79893e4701769c1a94c4
Jesse <3191955858@qq.com>
2025-07-31 20:58:37 +0800

    应用启动类。

---
+++ src/main/java/com/example/jesse/item_market/ItemMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.jesse.item_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 应用启动类。*/
+@SpringBootApplication
+public class ItemMarketApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(ItemMarketApplication.class, args);
+	}
+}
