138bffab3c6410bc5b59379aca0f43b347cf46e7
浩进 朱 <3191955858@qq.com>
2025-08-01 17:26:46 +0800

    Lua 脚本：用户销毁包裹中的某个武器。

--- src/main/resources/lua-script/destroyWeaponFromInventory.lua
+++ src/main/resources/lua-script/destroyWeaponFromInventory.lua
@@ -3,25 +3,30 @@
 
     KEYS:
         inventoryKey 用户包裹键（如：inventories:114935169325609268）
+        userKey      用户键（如：users:114934523722107784）
 
     ARGV:
         uuid        用户 UUID
         weaponName  武器名
 ]]
 local inventoryKey = KEYS[1]
+local userKey      = KEYS[2]
 
 local uuid       = ARGV[1]
 local weaponName = ARGV[2]
 
 local timestamp = redis.call('TIME')[1]
 
+local userName = redis.call('HGET', userKey, "\"name\"")
+
 redis.call('LREM', inventoryKey, 1, weaponName)
 redis.call(
     'XADD',
     'inventories:log', '*',
-    'event', 'WEAPON_DESTORY',
+    'event', 'WEAPON_DESTROY',
     'uuid', uuid,
-    'user-name', '---',
+    'user-name', userName,
+    'weapon-name', weaponName,
     'amount', '1',
     'timestamp', timestamp
 )
