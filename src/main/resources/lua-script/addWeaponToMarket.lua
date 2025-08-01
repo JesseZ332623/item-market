f16c96214144155b9eb26f3b2142bffd074f540b
浩进 朱 <3191955858@qq.com>
2025-08-01 17:26:28 +0800

    Lua 脚本：用户将武器上架至市场。

--- src/main/resources/lua-script/addWeaponToMarket.lua
+++ src/main/resources/lua-script/addWeaponToMarket.lua
@@ -5,6 +5,7 @@
         weaponHashKey           挂在市场上的武器键（如：market:weapon-market:weapons:1985f067af74d6d）
         weaponPriceZsetKey      挂在市场上的武器价格键（market:weapon-market:weapon-price）
         sellerInventoryListKey  卖家包裹键（如：inventories:114935169325609268）
+        userKey                 用户键（如：users:114934523722107784）
 
     ARGV:
         weaponUUID 武器 ID
@@ -16,6 +17,7 @@
 local weaponHashKey          = KEYS[1]
 local weaponPriceZsetKey     = KEYS[2]
 local sellerInventoryListKey = KEYS[3]
+local userKey                = KEYS[4]
 
 local weaponUUID  = ARGV[1]
 local sellerUUID  = ARGV[2]
@@ -23,6 +25,8 @@ local weaponName  = ARGV[3]
 local weaponPrice = ARGV[4]
 
 local timestamp = redis.call('TIME')[1]
+
+local userName = redis.call('HGET', userKey, "\"name\"")
 
 -- 从用户包裹中移除指定武器
 if redis.call('LREM', sellerInventoryListKey, 1, weaponName) == 0
@@ -36,6 +40,7 @@ redis.call(
     'inventories:log', '*',
     'event', 'WEAPON_OUTBOUND',
     'uuid', sellerUUID,
+    'user-name', userName,
     'weapon-name', weaponName,
     'amount', '1',
     'timestamp', timestamp
@@ -43,7 +48,7 @@ redis.call(
 
 redis.call(
     'HSET', weaponHashKey,
-    'weapon-name', weaponName, 'seller', sellerUUID
+    '"weapon-name"', weaponName, '"seller"', sellerUUID
 )
 redis.call(
     'ZADD', weaponPriceZsetKey,
