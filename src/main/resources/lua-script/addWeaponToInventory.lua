d534bd4aafe20d420f1107e9e55e458586637090
浩进 朱 <3191955858@qq.com>
2025-08-01 17:26:20 +0800

    Lua 脚本：为用户的包裹添加一件武器。

--- src/main/resources/lua-script/addWeaponToInventory.lua
+++ src/main/resources/lua-script/addWeaponToInventory.lua
@@ -2,25 +2,30 @@
     为用户的包裹添加一件武器。
 
     KEYS:
-        userKey 用户键（如：users:114934523722107784）
+        userKey      用户键（如：users:114934523722107784）
+        inventoryKey 用户包裹键（如：inventories:114934523722107784）
 
     ARGV:
         weapon 武器类型（Broadsword、Mace、Halberd 等）
 ]]
-local userKey = KEYS[1]
-local weapon  = ARGV[1]
+local userKey      = KEYS[1]
+local inventoryKey = KEYS[2]
 
-local uuid    = string.match(userKey, ":([^:]+)")
+local weapon       = ARGV[1]
+
+local uuid     = string.match(inventoryKey, ":([^:]+)")
+local userName = redis.call('HGET', userKey, "\"name\"")
 
 local timestamp = redis.call('TIME')[1]
 
-redis.call('RPUSH', userKey, weapon)
+redis.call('RPUSH', inventoryKey, weapon)
 redis.call(
     'XADD',
     'inventories:log', '*',
     'event', 'WEAPON_INBOUND',
     'uuid', uuid,
-    'user-name', '---',
+    'user-name', userName,
+    'weapon-name', weapon,
     'amount', '1',
     'timestamp', timestamp
 )
