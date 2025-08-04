311e1fc9a0705d379282a05cd4a129d3e8d2fd9f
浩进 朱 <3191955858@qq.com>
2025-08-04 16:24:07 +0800

    项目 Redis Key 组合工具类。

--- src/main/java/com/example/jesse/item_market/utils/KeyConcat.java
+++ src/main/java/com/example/jesse/item_market/utils/KeyConcat.java
@@ -20,11 +20,14 @@ final public class KeyConcat
     /** 用户哈希的用户资金字段名。*/
     public static final String USER_FUNDS_FIELD = "funds";
 
+    /** 用户联系人数据键前缀。*/
+    public static final String USER_CONTACT_PREFIX = "contact:";
+
     /** 用户包裹数据键前缀。*/
     public static final String INVENTORY_PREFIX = "inventories:";
 
     /** 武器市场数据键前缀。*/
-    public static final String MARKET_PREFIX     = "market:weapon-market:";
+    public static final String MARKET_PREFIX = "market:weapon-market:";
 
     /** 武器市场的价格表数据键。*/
     public static final String WEAPON_PRICE_ZSET = "market:weapon-market:weapon-price";
@@ -32,9 +35,17 @@ final public class KeyConcat
     /** 组合用户数据键。（示例：users:114940680399943670）*/
     @Contract(pure = true)
     public static @NotNull String
-    getUserKey(String uuid) {
-        return USER_PREFIX + uuid;
-    }
+    getUserKey(String uuid) { return USER_PREFIX + uuid; }
+
+    /** 组合用户最近联系人键。*/
+    @Contract(pure = true)
+    public static @NotNull String
+    getContactKey(String uuid) { return USER_CONTACT_PREFIX + uuid; }
+
+    /** 组合用户最近联系人日志键。*/
+    @Contract(pure = true)
+    public static @NotNull String
+    getContactLogKey() { return USER_CONTACT_PREFIX + "log"; }
 
     /** 组合用户包裹键。（示例：inventories:114940680399943670）*/
     @Contract(pure = true)
