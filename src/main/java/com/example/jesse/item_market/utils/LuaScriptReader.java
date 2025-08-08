684bb3e0d1025032d430d58ae6036050aa9b4f53
浩进 朱 <3191955858@qq.com>
2025-08-08 18:12:26 +0800

    Redis Lua 脚本读取器实现（可以在开发和测试环境自由切换）。

--- src/main/java/com/example/jesse/item_market/utils/LuaScriptReader.java
+++ src/main/java/com/example/jesse/item_market/utils/LuaScriptReader.java
@@ -5,6 +5,7 @@ import com.example.jesse.item_market.utils.exception.LuaScriptOperatorFailed;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,16 +27,33 @@ import static java.lang.String.format;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 final public class LuaScriptReader
 {
+    /** 本项目 Lua 脚本根目录。*/
+    private static final String
+    LUA_SCRIPT_CLASSPATH_PREFIX = "lua-script/";
+
     /** 本项目 Lua 脚本路径所在。*/
     @Value("${app.lua-script-path}")
     private String luaScriptPath;
 
+    /** 脚本的读取模式。*/
+    @Value("${app.lua-script-mode}")
+    private String scriptMode;
+
+    /** 从文件系统中读取脚本时用于拼合脚本路径。*/
     public @NotNull Path
     getFullScriptPath(
         @NotNull LuaScriptOperatorType operatorType,
         String luaScriptName
     )
     {
+        /* 对于从 JAR 中的相对路径获取资源的情况下，这个方法不可用。*/
+        if ("classpath".equals(scriptMode))
+        {
+            throw new UnsupportedOperationException(
+                "Path operator not supported in <classpath> mode!"
+            );
+        }
+
         switch (operatorType)
         {
             case USER_OPERATOR ->
@@ -71,27 +90,75 @@ final public class LuaScriptReader
         }
     }
 
+    /** 从文件系统中加载脚本。（开发、测试时用）*/
+    @Contract("_, _ -> new")
+    private @NotNull
+    DefaultRedisScript<LuaOperatorResult>
+    loadFromFileSystem(LuaScriptOperatorType operatorType, String luaScriptName) throws IOException
+    {
+        Path scriptPath
+            = this.getFullScriptPath(operatorType, luaScriptName);
+
+        if (!Files.exists(scriptPath))
+        {
+            throw new LuaScriptOperatorFailed(
+                format("Lua script: %s not found!", luaScriptName), null
+            );
+        }
+
+        return new DefaultRedisScript<>(
+            Files.readString(scriptPath, StandardCharsets.UTF_8),
+            LuaOperatorResult.class
+        );
+    }
+
+    /** 从 JAR 中的相对路径加载脚本（生产环境用）。*/
+    @Contract("_, _ -> new")
+    private @NotNull
+    DefaultRedisScript<LuaOperatorResult>
+    loadFromClassPath(@NotNull LuaScriptOperatorType operatorType, String luaScriptName) throws IOException
+    {
+            String classpathPath
+                = LUA_SCRIPT_CLASSPATH_PREFIX +
+                operatorType.getTypeName()  +
+                "/"                         +
+                luaScriptName;
+
+            try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(classpathPath))
+            {
+                if (inputStream == null)
+                {
+                    throw new LuaScriptOperatorFailed(
+                        format("Lua script: %s not found in classpath!",
+                            classpathPath), null);
+                }
+
+                String scriptContent
+                    = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+
+                return new DefaultRedisScript<>(scriptContent, LuaOperatorResult.class);
+            }
+    }
+
+    /**
+     * 根据配置，从不同的源读取脚本。
+     *
+     * @param operatorType  Lua 脚本类型
+     * @param luaScriptName Lua 脚本名
+     */
     public @NotNull Mono<DefaultRedisScript<LuaOperatorResult>>
     fromFile(LuaScriptOperatorType operatorType, String luaScriptName)
     {
         return Mono.fromCallable(() -> {
-
-            Path scriptPath
-                = this.getFullScriptPath(operatorType, luaScriptName);
-
-            if (!Files.exists(scriptPath))
+            if ("classpath".equals(this.scriptMode))
             {
-                throw new LuaScriptOperatorFailed(
-                    format("Lua script: %s not found!", luaScriptName), null
-                );
+                return this.loadFromClassPath(operatorType, luaScriptName);
             }
-
-            return new DefaultRedisScript<>(
-                Files.readString(scriptPath, StandardCharsets.UTF_8),
-                LuaOperatorResult.class
-            );
-        })
-        .onErrorMap(
+            else
+            {
+                return this.loadFromFileSystem(operatorType, luaScriptName);
+            }
+        }).onErrorMap(
             IOException.class,
             (exception) ->
                 new LuaScriptOperatorFailed(
