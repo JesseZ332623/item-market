ffef0be70cb94d272b228853d6c41636991cbfcd
浩进 朱 <3191955858@qq.com>
2025-08-11 18:15:39 +0800

    要读取的 Lua 脚本的操作类型枚举。

--- src/main/java/com/example/jesse/item_market/utils/LuaScriptOperatorType.java
+++ src/main/java/com/example/jesse/item_market/utils/LuaScriptOperatorType.java
@@ -8,7 +8,8 @@ public enum LuaScriptOperatorType
     USER_OPERATOR("user-operator"),
     MARKET_OPERATOR("market-operator"),
     GUILD_OPERATOR("guild-operator"),
-    ACQUIRE_OPERATOR("lock-operator");
+    LOCK_OPERATOR("lock-operator"),
+    SEMAPHORE_OPERATOR("semaphore-operator");
 
     @Getter
     final String typeName;
