ab1c270c288f44fd1fdd824ed055efbaa8c44045
浩进 朱 <3191955858@qq.com>
2025-08-12 17:05:43 +0800

    向指定用户发送邮件的内容实体。

--- src/main/java/com/example/jesse/item_market/email/dto/EmailContent.java
+++ src/main/java/com/example/jesse/item_market/email/dto/EmailContent.java
@@ -3,6 +3,7 @@ package com.example.jesse.item_market.email.dto;
 import jakarta.annotation.Nullable;
 import lombok.*;
 import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
 
 import java.nio.file.Path;
 import java.time.Duration;
@@ -30,30 +31,33 @@ public class EmailContent
      * @param varifyCode 验证码
      * @param expired    验证码有效期（一般从属性中获取）
      *
-     * @return 验证码邮件内容
+     * @return 发布验证码邮件内容的 Mono
      */
-    public static @NotNull EmailContent
+    public static @NotNull Mono<EmailContent>
     fromVarify(
         String userName, String userEmail,
         String varifyCode, @NotNull Duration expired)
     {
-        EmailContent emailContent = new EmailContent();
+        return
+        Mono.fromCallable(() -> {
+            EmailContent emailContent = new EmailContent();
 
-        emailContent.setTo(userEmail);
-        emailContent.setSubject("用户：" + userName + " 请查收您的验证码。");
-        emailContent.setTextBody(
-            format(
-                "用户：%s 您的验证码是：[%s]，" +
-                "请在 %s 分钟内完成验证，超过 %s 分钟后验证码自动失效！",
-                userName, varifyCode,
-                expired.toMinutes(), expired.toMinutes()
-            )
-        );
+            emailContent.setTo(userEmail);
+            emailContent.setSubject("用户：" + userName + " 请查收您的验证码。");
+            emailContent.setTextBody(
+                format(
+                    "用户：%s 您的验证码是：[%s]，" +
+                        "请在 %s 分钟内完成验证，超过 %s 分钟后验证码自动失效！",
+                    userName, varifyCode,
+                    expired.toMinutes(), expired.toMinutes()
+                )
+            );
 
-        // 验证码邮件不需要附件内容
-        emailContent.setAttachmentPath(null);
+            // 验证码邮件不需要附件内容
+            emailContent.setAttachmentPath(null);
 
-        return emailContent;
+            return emailContent;
+        });
     }
 
     /**
@@ -65,24 +69,27 @@ public class EmailContent
      * @param message    邮件正文
      * @param attachment 附件路径
      *
-     * @return 带附件的邮件内容
+     * @return 发布带附件的邮件内容的 Mono
      */
-    public static @NotNull EmailContent
+    public static @NotNull Mono<EmailContent>
     formWithAttachment(
         String userName, String userEmail,
         String subject, String message, String attachment)
     {
-        EmailContent emailContent = new EmailContent();
+        return
+        Mono.fromCallable(() -> {
+            EmailContent emailContent = new EmailContent();
 
-        emailContent.setTo(userEmail);
-        emailContent.setSubject("用户：" + userName + " " + subject);
-        emailContent.setTextBody(message);
+            emailContent.setTo(userEmail);
+            emailContent.setSubject("用户：" + userName + " " + subject);
+            emailContent.setTextBody(message);
 
-        Path attachmentPath
-            = Path.of(attachment).normalize();
+            Path attachmentPath
+                = Path.of(attachment).normalize();
 
-        emailContent.setAttachmentPath(attachmentPath.toString());
+            emailContent.setAttachmentPath(attachmentPath.toString());
 
-        return emailContent;
+            return emailContent;
+        });
     }
 }
