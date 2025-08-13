9dd6cd6e7b5c9ff85b6f5581533248e67a57028a
浩进 朱 <3191955858@qq.com>
2025-08-13 18:22:26 +0800

    邮件任务类型枚举。

---
+++ src/main/java/com/example/jesse/item_market/email_send_task/dto/TaskType.java
@@ -0,0 +1,18 @@
+package com.example.jesse.item_market.email_send_task.dto;
+
+import lombok.Getter;
+
+/** 邮件任务类型枚举。*/
+public enum TaskType
+{
+    /** 需要延迟执行的 */
+    DELAY_TASK("delay-task"),
+
+    /** 不需要延迟执行，但任务是有优先级的 */
+    PRIORITY_TASK("priority-task");
+
+    @Getter
+    final String type;
+
+    TaskType(String t) { this.type = t; }
+}
