dd9315b5c67514999c6a2a26460fa5ecfa4dd141
浩进 朱 <3191955858@qq.com>
2025-08-13 18:20:44 +0800

    邮件任务的封装。

---
+++ src/main/java/com/example/jesse/item_market/email_send_task/dto/EmailTaskDTO.java
@@ -0,0 +1,51 @@
+package com.example.jesse.item_market.email_send_task.dto;
+
+import com.example.jesse.item_market.email.dto.EmailContent;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+/** 邮件任务的封装。*/
+@Getter
+@ToString
+@JsonTypeInfo(
+    use      = JsonTypeInfo.Id.CLASS,        // 使用完全限定类名作为类型标识
+    include  = JsonTypeInfo.As.PROPERTY,     // 作为单独属性包含
+    property = "@class"                      // 类型信息的属性名
+)
+/* 只要任务的唯一标识完全相同，它们便是同一个任务 */
+@EqualsAndHashCode(of = {"taskIdentifier"})
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class EmailTaskDTO
+{
+    /** 邮件任务唯一标识符（在调用 create() 时自动生成）*/
+    private String taskIdentifier;
+
+    /** 邮件任务优先级。*/
+    private String priorityName;
+
+    /** 邮件任务优先级分数。*/
+    private double priorityScore;
+
+    /** 邮件内容。*/
+    private EmailContent content;
+
+    /** 创建一个邮件任务。*/
+    public static @NotNull Mono<EmailTaskDTO>
+    create(@NotNull TaskPriority priority, EmailContent content)
+    {
+        return
+        Mono.just(
+            new EmailTaskDTO(
+                UUID.randomUUID().toString(),
+                priority.getPriorityName(),
+                priority.getPriorityScore(),
+                content
+            )
+        );
+    }
+}
