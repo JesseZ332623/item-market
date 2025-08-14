a2fda026eeb36669b3ee7199f2d76eb460cbde79
浩进 朱 <3191955858@qq.com>
2025-08-14 13:28:32 +0800

    封装了 javax.mail 库的响应式邮件发送器接口。

--- src/main/java/com/example/jesse/item_market/email/service/EmailSenderInterface.java
+++ src/main/java/com/example/jesse/item_market/email/service/EmailSenderInterface.java
@@ -1,6 +1,7 @@
 package com.example.jesse.item_market.email.service;
 
 import com.example.jesse.item_market.email.dto.EmailContent;
+import com.example.jesse.item_market.email.exception.EmailException;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
@@ -11,6 +12,8 @@ public interface EmailSenderInterface
      * 外部可调用的发送邮件的方法。
      *
      * @param emailContent 邮件内容
+     *
+     * @throws EmailException 在邮件发送失败后抛出
      *
      * @return 表示操作是否正确完成的响应式流
      */
