a3a9dd4b4ec43fec7865dcf1fc7c96b7e8d836ae
浩进 朱 <3191955858@qq.com>
2025-08-15 10:45:32 +0800

    邮件任务的优先级枚举。（更细化的优先级）

--- src/main/java/com/example/jesse/item_market/email_send_task/dto/TaskPriority.java
+++ src/main/java/com/example/jesse/item_market/email_send_task/dto/TaskPriority.java
@@ -5,9 +5,13 @@ import lombok.Getter;
 /** 邮件任务的优先级枚举。*/
 public enum TaskPriority
 {
-    LOW("low", 30.00),
-    MEIDUM("meidum", 60.00),
-    HIGH("high", 90.00);
+    LOW("low", 25.00),
+    MEDIUM_LOW("medium_low", 50.00),
+    MEDIUM("medium", 60.00),
+    MEDIUM_HIGH("medium_high", 70.00),
+    HIGH("high", 80.00),
+    VERY_HIGH("very_high", 90.00),
+    CRITICAL("critical", 100.00);
 
     @Getter
     final String priorityName;
