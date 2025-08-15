5d5df1f8d2207c4adad5960018ba7ef32112216d
浩进 朱 <3191955858@qq.com>
2025-08-15 15:35:08 +0800

    应用启动类。（开启定时任务注解）

--- src/main/java/com/example/jesse/item_market/ItemMarketApplication.java
+++ src/main/java/com/example/jesse/item_market/ItemMarketApplication.java
@@ -2,8 +2,10 @@ package com.example.jesse.item_market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /** 应用启动类。*/
+@EnableScheduling
 @SpringBootApplication
 public class ItemMarketApplication
 {
