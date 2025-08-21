daa5b79ef3530e2884253eeefae059ce07aed111
浩进 朱 <3191955858@qq.com>
2025-08-21 18:08:09 +0800

    IPV4 转换器。

--- src/main/java/com/example/jesse/item_market/location_search/uitls/Ipv4Converter.java
+++ src/main/java/com/example/jesse/item_market/location_search/uitls/Ipv4Converter.java
@@ -8,23 +8,39 @@ public class Ipv4Converter
 {
     @Contract(pure = true)
     public static @NotNull String
-    IntToIPV4(int ipNumber)
+    intToIPv4(long ipNumber)
     {
-        return
-        ((ipNumber >> 24) & 0xFF) + "." +
-        ((ipNumber >> 16) & 0xFF) + "." +
-        ((ipNumber >> 8)  & 0xFF) + "." +
-        (ipNumber         & 0xFF);
+        byte[] ipBytes = new byte[4];
+        int    offset  = 24;
+        StringBuilder resBuilder = new StringBuilder();
+
+        for (int index = 0; index < ipBytes.length; ++index)
+        {
+            ipBytes[index] = (byte) ((ipNumber >> offset) & 0xFF);
+            offset         = offset - 8;
+        }
+
+        for (int index = 0; index < ipBytes.length; ++index)
+        {
+            resBuilder.append(ipBytes[index] & 0xFF);
+
+            if (index < 3) {
+                resBuilder.append(".");
+            }
+        }
+
+        return resBuilder.toString();
+
     }
 
-    public static int
-    IPV4ToInt(@NotNull String ipv4String)
+    public static long
+    ipv4ToInt(@NotNull String ipv4String)
     {
         if (ipv4String.trim().isEmpty()) {
             throw new IllegalArgumentException("IPv4 address not be empty!");
         }
 
-        int result = 0;
+        long result = 0;
 
         String[] ipParts
             = ipv4String.trim().split("\\.");
@@ -33,12 +49,20 @@ public class Ipv4Converter
             throw new IllegalArgumentException("IPv4 address must include 4 parts!");
         }
 
+
         for (String ipPart : ipParts)
         {
             if (!ipPart.matches("\\d+"))
             {
                 throw new IllegalArgumentException(
                     "IPv4 address include non-number character!"
+                );
+            }
+
+            if (ipPart.length() > 1 && ipPart.startsWith("0"))
+            {
+                throw new IllegalArgumentException(
+                    "Leading zeros are not allowed in IP parts."
                 );
             }
 
@@ -57,7 +81,7 @@ public class Ipv4Converter
             if (partNumber < 0 || partNumber > 255)
             {
                 throw new IllegalArgumentException(
-                    "Value of IP part number must betwwen 0 and 255!"
+                    "Value of IP part number must between 0 and 255!"
                 );
             }
 
