0256f1cf4cbc2e82df007b26fc6d9ed9e38918d8
浩进 朱 <3191955858@qq.com>
2025-09-18 16:24:44 +0800

    应用启动类。

--- src/main/java/com/example/jesse/item_market/ItemMarketApplication.java
+++ src/main/java/com/example/jesse/item_market/ItemMarketApplication.java
@@ -2,6 +2,7 @@ package com.example.jesse.item_market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -9,6 +10,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableKafka
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan(
+    basePackages = {
+        "com.example.jesse.item_market",
+        "io.github.jessez332623"
+    }
+)
 public class ItemMarketApplication
 {
 	public static void main(String[] args) {
