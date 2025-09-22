75a3b520f32f38a778e569fefca2e32e9392dff1
浩进 朱 <3191955858@qq.com>
2025-09-22 16:18:56 +0800

    表示 HATEOAS 元数据中的某一条链接的 POJO。

--- src/main/java/com/example/jesse/item_market/response/pojo/Link.java
+++ src/main/java/com/example/jesse/item_market/response/pojo/Link.java
@@ -1,17 +1,16 @@
-package com.example.jesse.item_market.response;
+package com.example.jesse.item_market.response.pojo;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 
-/**
- * 这个类用来表示 HATEOAS 元数据中的某一条链接。
- */
+/** 表示 HATEOAS 元数据中的某一条链接的 POJO。*/
 @Data
 @Slf4j
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class Link
 {
